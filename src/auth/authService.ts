7275e55071ceb776d6f9354488507f2fe1b9508a
suwandre <suwandresukijat@gmail.com>
2023-04-01 11:40:38 +0200

    replaced uri and domain for authService

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -20,7 +20,7 @@ export async function requestMessage({
   chain: string;
   networkType: 'evm';
 }) {
-  // const url = new URL(config.SERVER_URL);
+  const url = new URL(config.SERVER_URL);
   const now = new Date();
   const expirationTime = new Date(now.getTime() + EXPIRATION_TIME);
 
@@ -28,8 +28,8 @@ export async function requestMessage({
     address,
     chain,
     networkType,
-    domain: 'nbcompany',
-    uri: 'https://nbcompany.io',
+    domain: url.hostname,
+    uri: url.toString(),
     statement: STATEMENT,
     notBefore: now.toISOString(),
     expirationTime: expirationTime.toISOString(),
